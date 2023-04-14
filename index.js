f974524a47d8b6e740c9a554a4c3ed6961d61650
Emily Bitelo <129232644+gabibitelo@users.noreply.github.com>
2023-04-13 22:04:43 -0300

    aula 13
    aula 13

--- index.js
+++ index.js
@@ -1,21 +1,102 @@
+//12/04//
+
+//exercicios//
+
+ //1 atividade//
+
 //Graus Fahrenheit p/kelvin
 //(KELVIN) = (GRAUS_FAHRENHEIT -32)*(5/9) + 273.15
 
 //GRAUS CELSIUS P/FAHRENHEIT
 //(GRAUS_FAHRENHEIT) = (GRAUS CELCIUS)*(9/5) + 32
 
-let kelvin = (77-32)*(5/9) + 273.15
-console.log(kelvin)
- 
-//*b) calcule e mostre o valor de 80 C em F, mostrando a unidade no console tabém
-let Fahrenheit = (80)*(9/5) + 32
-console.log(Fahrenheit)
+//2 atividade//
 
-//calcule e mostre o valor de 30 c em F e K mostrando as unidades no console
-let Fahrenheit1 = (30)*(9/5) + 32
-console.log(Fahrenheit1)
+// let kelvin = (77-32)*(5/9) + 273.15
+// console.log(kelvin)
 
-let kelvin1 = (Fahrenheit1-32)*(5/9) + 273.15
-console.log(kelvin1)
+// //3 atividade//
+
+// //*b) calcule e mostre o valor de 80 C em F, mostrando a unidade no console tabém
+// let Fahrenheit = (80)*(9/5) + 32
+// console.log(Fahrenheit)
+
+// //4 atividade//
+
+// //calcule e mostre o valor de 30 c em F e K mostrando as unidades no console
+// let Fahrenheit1 = (30)*(9/5) + 32
+// console.log(Fahrenheit1)
+
+// let kelvin1 = (Fahrenheit1-32)*(5/9) + 273.15
+// console.log(kelvin1)
 
 //altere o ultimo item para que o usario insira o valor em graus Celsios que ele deseja converter
+ 
+
+
+
+
+//13/04/23//
+
+//ATIVIDADES//
+
+//*A) solicitar o nome do usario, 3 notas, calcular a média
+/*let nome = prompt('digite seu nome: ')
+let emly1 = Number(prompt('informe a primeira nota '));
+let emily2 = Number(prompt('informe a segunda nota'));
+let emily3 = Number(prompt('informe a terceira nota'));
+let soma = emily1 + emily2 + emily3
+let media = soma / 3
+*/
+//console.log
+//console.log(nome+ " suas notas foram: " +emily1+" , " + emily2 +" e " emily3)
+
+//solicitar nome do usario
+
+// let nome = prompt("digite seu nome: "); 
+
+//  //3 notas//
+// Number(prompt("digite a nota 1: "));
+// Number(prompt("digite a nota 2: "));
+// Number(prompt("digite a nota 3: "));
+
+// const nota1 = Number(prompt("digite a nota 1: "));
+// const nota2 = Number(prompt("digite a nota 2: "));
+// const nota3 = Number(prompt("digite a nota 3: "));
+
+// //calcular a media //
+// let soma = nota1 + nota2 + nota3;
+// let media = soma / 3 
+
+// //informar nome do usario
+
+// console.log(nome, media, media >= 6)
+
+// if ( media >= 6) {
+    // alert(nome, ', sua media foi 1 false' )
+
+
+
+//B / solicitar um numero para o usario.
+// validar se é par e informar o resultado
+
+// let numero = Number(prompt('imforme um numero: '))
+// if(numero%2===0){
+//     alert('o numero '+ numero +' é par')
+//     console.log('o numero '+ numero +' é par')
+// }else{
+//     alert('o numero '+numero + " é ímpar.")
+
+//     console.log('o numero '+numero + " é ímpar.")
+// }
+
+// C / solicitar 2 numeros para o usario exibir o maior
+
+// let num1 = Number(prompt('digite um numero: '))
+// let num2 = Number(prompt('digite outro numero: '))
+// if (num1 > num2) {
+//     alert(num1)
+// } else {              
+//     alert(num2)
+// }      
+
